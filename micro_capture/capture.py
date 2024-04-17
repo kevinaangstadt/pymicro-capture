bb991fca420af8dea023e34617fcbbddc4542dd7
Kevin Angstadt <kangstadt@stlawu.edu>
2024-04-17 15:02:12 -0400

    testing program

---
+++ micro_capture/capture.py
@@ -0,0 +1,9 @@
+import logging
+
+from leica import LeicaEZ4HD
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+microscope = LeicaEZ4HD()
+
+microscope.capture_image("test.jpg")
